3f61a2640bbd3f38dab10b615fd589accc88e0db
5tarlight <developerstarlight@gmail.com>
2019-05-12 01:59:26 +0900

    auto command detection

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,23 +1,28 @@
 import { Client, Message } from 'discord.js'
 import { info } from '../SLog';
 import { bot } from '../config/const.json'
+import Command from './command'
 
 class Bot {
   static client: Client
+  static prefix: string = '$'
 
   static login(): void {
     Bot.client = new Client()
+    Command.init()
 
     Bot.client.on('ready', () => {
       info(`Logged in as ${Bot.client.user.tag}`)
     })
     
     Bot.client.on('message', (msg: Message) => {
-      if(msg.content === `$ping`) {
-        msg.channel.send(`**${Bot.client.ping}** ms`)
-      }
-    })
+      const cmd = msg.content.slice(Bot.prefix.length, msg.content.length).split(' ')[0]
+      const arg = msg.content.slice(Bot.prefix.length, msg.content.length).split(' ')
+      const args = arg.slice(1, msg.content.slice(Bot.prefix.length, msg.content.length).split(' ').length)
+
+      Command.execute(cmd, Bot.client, msg, args)
     
+    })
     Bot.client.login(bot.token)
   }
 }
