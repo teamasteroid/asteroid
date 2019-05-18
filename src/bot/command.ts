1e727132c69487aeab06e1aceedb9379218f4c8c
5tarlight <developerstarlight@gmail.com>
2019-05-18 11:08:52 +0900

    command listener

--- src/bot/command.ts
+++ src/bot/command.ts
@@ -1,21 +1,23 @@
 import { Client, Message } from "discord.js";
 import { Map } from 'immutable'
-import { Ping } from './cmd'
+import { Ping, Uptime } from './cmd'
+import CE from "./cmd/CE";
 
 type cmdFuncType = (client: Client, msg: Message, args?: string[]) => void
 
 class Command {
-  static cmds: Map<string, cmdFuncType> = Map({})
+  static cmds: Map<string, CE> = Map({})
 
   static init() {
-    Command.cmds = Command.cmds.set('ping', Ping.ping)
+    Command.cmds = Command.cmds.set('ping', new Ping())
+    Command.cmds = Command.cmds.set('uptime', new Uptime())
   }
 
   static execute(command: string, client: Client, msg: Message, args?: string[]): void {
     const cmdExe = Command.cmds.get(command)
 
     if (cmdExe) {
-      cmdExe(client, msg, args)
+      cmdExe.command(client, msg, args)
     } else
       return
   }
