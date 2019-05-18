c6627fdf8a9d2444ed7b72d78475a44c6b6e7c8f
5tarlight <developerstarlight@gmail.com>
2019-05-18 11:08:03 +0900

    extends class

--- src/bot/cmd/ping.ts
+++ src/bot/cmd/ping.ts
@@ -1,9 +1,10 @@
 import { Client, Message, RichEmbed } from "discord.js";
 import { info } from "../../SLog";
 import Bot from '../bot'
+import CE from "./CE";
 
-class Ping {
-  static ping(client: Client, msg: Message, args?: string[]): void {
+class Ping extends CE{
+  command(client: Client, msg: Message, args?: string[]): void {
     const embed = new RichEmbed()
       .setTitle(`**${Math.round(client.ping)}** ms`)
       .setDescription(`핑은 봇이 돌아가는 서버에서 측정되었습니다.`)
@@ -12,7 +13,7 @@ class Ping {
     msg.channel.send(embed)
   }
 
-  static getPing(): number {
+  getPing(): number {
     return Math.round(Bot.client.ping)
   }
 }
