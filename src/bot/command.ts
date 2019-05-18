0290ad7bbd323fd6ed2a2ac9504b63c65d8bc999
5tarlight <developerstarlight@gmail.com>
2019-05-18 18:17:25 +0900

    improved listener

--- src/bot/command.ts
+++ src/bot/command.ts
@@ -1,6 +1,6 @@
 import { Client, Message } from "discord.js";
 import { Map } from 'immutable'
-import { Ping, Uptime } from './cmd'
+import { Ping, Uptime, ServerList } from './cmd'
 import CE from "./cmd/CE";
 
 type cmdFuncType = (client: Client, msg: Message, args?: string[]) => void
@@ -9,10 +9,18 @@ class Command {
   static cmds: Map<string, CE> = Map({})
 
   static init() {
-    Command.cmds = Command.cmds.set('ping', new Ping())
-    Command.cmds = Command.cmds.set('핑', new Ping())
-    Command.cmds = Command.cmds.set('uptime', new Uptime())
-    Command.cmds = Command.cmds.set('업타임', new Uptime())
+    Command.set('ping', new Ping())
+    Command.set('핑', new Ping())
+    Command.set('uptime', new Uptime())
+    Command.set('업타임', new Uptime())
+    Command.set('serverlist', new ServerList())
+    Command.set('sl', new ServerList())
+    Command.set('서버리스트', new ServerList())
+    Command.set('서버', new ServerList())
+  }
+
+  static set(cmd: string, ce: CE) {
+    Command.cmds = Command.cmds.set(cmd, ce)
   }
 
   static execute(command: string, client: Client, msg: Message, args?: string[]): void {
