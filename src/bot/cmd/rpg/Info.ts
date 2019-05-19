299fc48103c7a109a8509bfb3b58275f47043a06
5tarlight <developerstarlight@gmail.com>
2019-05-19 23:30:38 +0900

    기획

---
+++ src/bot/cmd/rpg/Info.ts
@@ -0,0 +1,10 @@
+import CE from "../CE";
+import { Client, Message } from "discord.js";
+
+class Info extends CE {
+  command(client: Client, msg: Message, args: string[]) {
+    
+  }
+}
+
+export default Info
