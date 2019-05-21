4508e6f0deddbbf6490b88ffe8447231c5096248
5tarlight <developerstarlight@gmail.com>
2019-05-22 00:20:52 +0900

    improved db manager

--- src/bot/cmd/rpg/DB.ts
+++ src/bot/cmd/rpg/DB.ts
@@ -7,12 +7,12 @@ class DB {
   static conn: Connection
 
   static init() {
-    this.conn = createConnection(DBSetting.db)
+    DB.conn = createConnection(DBSetting.db)
     success('DB Connection Ready')
   }
 
   static query(query: string, values: any, callback: queryCallback) {
-    this.conn.query(query, values, callback)
+    DB.conn.query(query, values, callback)
   }
 }
 
