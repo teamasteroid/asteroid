24f6180050d19716c4b35308b2788222a2c75d8e
5tarlight <developerstarlight@gmail.com>
2019-05-28 18:44:20 +0900

    item info_temp

---
+++ src/bot/cmd/rpg/ItemInfo.ts
@@ -0,0 +1,31 @@
+import CE from "../CE";
+import { Client, Message, RichEmbed } from "discord.js";
+import * as items from './lib/item.json'
+
+class ItemInfo extends CE {
+  command(client: Client, msg: Message, args: string[]) {
+    if(!args || args.length < 1) {
+      const embed = new RichEmbed()
+        .setTitle('사용법')
+        .addField('$ii <아이템 id>', '해당 아이템의 정보를 봅니다.')
+        .addField('$ii <아이템 이름>', '해당 아이템의 정보를 봅니다 (추천하지 않음)')
+
+      msg.channel.send(embed)
+      return
+    }
+
+    const ii = args[0].toUpperCase()
+
+    if(items.RESOURCE["WOOD"]) {
+
+    }
+  }
+
+  desc = {
+    name: "iteminfo",
+    description: "아이템의 정보를 봅니다.",
+    aliases: ["ii", "아이템", "searchitem", "si", "about"]
+  }
+}
+
+export default ItemInfo
