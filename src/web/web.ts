e92152c9efe573f775c507bc4af9a91769447a9e
5tarlight <developerstarlight@gmail.com>
2019-06-02 10:49:03 +0900

    conmmentilize

--- src/web/web.ts
+++ src/web/web.ts
@@ -1,7 +1,10 @@
-import express, { Application, Router, static as ss, Request, Response } from "express";
+import express, { Application, Router, static as ss } from "express";
 import { success } from "../SLog";
 import { Portal } from './routes'
 import { oauth } from '../config/const.json'
+import { Strategy } from 'passport-discord'
+import passport from 'passport'
+import session from 'express-session'
 
 class Web {
   static app: Application
@@ -15,12 +18,47 @@ class Web {
     Web.app.set('views', __dirname + '/public/views')
     Web.app.set('view engine', 'pug')
 
-    const CLIENT_ID = oauth.CLIENT_ID
+    // const CLIENT_ID = oauth.CLIENT_ID
     const CLIENT_SECRET = oauth.CLIENT_SECRET
-    const REDIRECT = "/oauth/callback/discord"
+    // const REDIRECT = "http://5tarlight.kro.kr/oauth/callback/discord"
 
+    // const scopes = ['identify']
+
+    // passport.serializeUser(function(user, done) {
+    //   done(null, user);
+    // });
+    // passport.deserializeUser(function(obj, done) {
+    //   done(null, obj);
+    // });
+
+    // passport.use(new Strategy({
+    //   clientID: CLIENT_ID,
+    //   clientSecret: CLIENT_SECRET,
+    //   callbackURL: REDIRECT,
+    //   scope: scopes
+    // },
+    // (accessToken, refreshToken, profile, cb) => {
+    //   process.nextTick(() => cb(null, profile))
+    // }))
+
+    Web.app.use(session({
+      secret: CLIENT_SECRET,
+      resave: true,
+      saveUninitialized: false
+    }))
+    
+    // Web.app.use(passport.initialize())
+    // Web.app.use(passport.session())  
+    
+    // Web.router.route('/login').get(passport.authenticate('discord', { scope: scopes }), function(req, res) {})
+    // Web.router.route('/oauth/callback/discord').get(passport.authenticate('discord', { failureRedirect: '/login' }), (req, res) => { res.redirect('/') })
+    // Web.router.route('/logout').get((req, res) => {
+    //   req.logout()
+    //   res.redirect('/')
+    // })
+  
+    
     Web.router.route('/').get(Portal.join)
-
     Web.app.use('/', ss(__dirname + '/public'))
     Web.app.use(Web.router)
 
