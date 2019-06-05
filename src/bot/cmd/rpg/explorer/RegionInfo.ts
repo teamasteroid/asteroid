a2823aa4af45ad17814d9dc947592867f829d893
5tarlight <developerstarlight@gmail.com>
2019-06-05 23:07:19 +0900

    region check

--- src/bot/cmd/rpg/explorer/RegionInfo.ts
+++ src/bot/cmd/rpg/explorer/RegionInfo.ts
@@ -1,14 +1,25 @@
 import CE from "../../CE";
 import { Client, Message, RichEmbed } from "discord.js";
-import * as regions from '../lib/region.json'
+import RegionManager from "../lib/RegionManager";
 
 class RegionInfo extends CE {
   command(client: Client, msg: Message, args: string[]) {
-    if(args.length < 1) {
+    if(!args || args.length < 1) {
       const embed = new RichEmbed()
         .setTitle('사용법')
         .addField('$ri <지역 id>', '해당 지역의 정보를 봅니다.')
 
+      msg.channel.send(embed)
+      return
+    }
+
+    const regions = RegionManager.getItemList()
+
+    if(!regions.getIn([args[0]])) {
+      const embed = new RichEmbed()
+        .setTitle('해당 지역을 찾을 수 없습니다.')
+        .setColor('#ff3333')
+      
       msg.channel.send(embed)
       return
     }
