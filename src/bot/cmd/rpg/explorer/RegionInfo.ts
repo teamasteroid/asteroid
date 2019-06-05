8fed96954178fde32c8e206a67f6404abecd4e48
5tarlight <developerstarlight@gmail.com>
2019-06-05 22:53:56 +0900

    usage chacker

--- src/bot/cmd/rpg/explorer/RegionInfo.ts
+++ src/bot/cmd/rpg/explorer/RegionInfo.ts
@@ -1,9 +1,17 @@
 import CE from "../../CE";
-import { Client, Message } from "discord.js";
+import { Client, Message, RichEmbed } from "discord.js";
+import * as regions from '../lib/region.json'
 
 class RegionInfo extends CE {
   command(client: Client, msg: Message, args: string[]) {
+    if(args.length < 1) {
+      const embed = new RichEmbed()
+        .setTitle('사용법')
+        .addField('$ri <지역 id>', '해당 지역의 정보를 봅니다.')
 
+      msg.channel.send(embed)
+      return
+    }
   }
 
   desc = {
