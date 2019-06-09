378d5fbe11d684e7dde821a9d32c58ac5e58f978
5tarlight <developerstarlight@gmail.com>
2019-06-09 09:53:11 +0900

    get item

--- src/bot/cmd/rpg/lib/ItemManager.ts
+++ src/bot/cmd/rpg/lib/ItemManager.ts
@@ -1,5 +1,8 @@
 import * as items from './item.json'
 import { fromJS } from 'immutable';
+import { User } from 'discord.js';
+import DB from './DB.js';
+import { err as error} from '../../../../SLog'
 
 class ItemManager {
   static itemList: any
@@ -9,6 +12,23 @@ class ItemManager {
 
   static getItemList(): any {
     return ItemManager.itemList
+  }
+
+  static getUserItem(user: User): any {
+    DB.query('SELECT * FROM user WHERE id=?', [user.id], (err, results, fields) => {
+      if(err) {
+        error(err.stack || err.toString())
+        return null
+      }
+
+      if(results.length < 1) {
+        // Not Found
+        return null
+      } else {
+        // Found
+        return JSON.parse(results[0]['item'])
+      }
+    })
   }
 }
 
