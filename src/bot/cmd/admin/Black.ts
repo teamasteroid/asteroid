551d73d4982def1d5ea82cf228b7322b428d43a5
5tarlight <developerstarlight@gmail.com>
2019-08-05 08:38:14 +0900

    Discovered forgotten file and permission denied embed message

--- src/bot/cmd/admin/Black.ts
+++ src/bot/cmd/admin/Black.ts
@@ -4,13 +4,20 @@ import CE from "../CE";
 
 class Black extends CE{
   command(client: Client, msg: Message, args?: string[]): void {
-    
+    if(msg.author.id !== '352755226224361482') {
+      const embed = new RichEmbed()
+        .setTitle('Error : Permission Denied')
+        .setColor('#ff3333')
+      
+      msg.channel.send(embed)
+      return
+    }
   }
 
   desc = {
     name: "black",
     description: "해당 사용자를 블랙리스트에 추가합니다.",
-    aliases: ['핑', 'pong']
+    aliases: ['블랙', '블랙리스트']
   }
 }
 
