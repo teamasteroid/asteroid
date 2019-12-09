5b12bccdcc9d86c1212a0fe2ce0b81034f4840c9
Kim Kyu San <developerstarlight@gmail.com>
2019-12-09 14:55:33 +0900

    event handler super class

---
+++ event/EventHandler.js
@@ -0,0 +1,13 @@
+const Logger = require('discord.js')
+
+class EventHandler {
+  constructor(eventName) {
+    this.eventName = eventName
+  }
+  
+  bind(client, e) {
+    Logger.warn('No Handler given')
+  }
+}
+
+module.exports = EventHandler
