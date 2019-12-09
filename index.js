008aba0ef8921308c97c325fc49dcca2597dc3cc
Kim Kyu San <developerstarlight@gmail.com>
2019-12-09 15:00:14 +0900

    beautify

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Logger = require('korean-logger')
 const Core = require('./data/core.json')
 const { ShardingManager } = require('discord.js')
+
 const shardManager = new ShardingManager('./bot.js', {
   token: Core.bot.token,
   totalShards: Core.bot.shard.count,
