ceacc8da04d2cbc49277401e19cf6ff98b6db23a
Kim Kyu San <developerstarlight@gmail.com>
2019-12-14 11:14:06 +0900

    database util

---
+++ utils/Database.js
@@ -0,0 +1,28 @@
+const { createConnection } = require('mysql');
+const Core = require('../data/core.json')
+
+class Database {
+    constructor() {
+        this.connection = createConnection(Core.db);
+    }
+    query(sql, args) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, args, (err, rows) => {
+                if (err)
+                    return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+    close() {
+        return new Promise((resolve, reject) => {
+            this.connection.end(err => {
+                if (err)
+                    return reject(err);
+                resolve();
+            });
+        });
+    }
+}
+
+module.exports = Database
