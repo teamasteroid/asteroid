b4e849de564252b24b6b8b3d37468e0c4c744b14
Kim Kyu San <developerstarlight@gmail.com>
2019-12-23 16:42:42 +0900

    embed util

--- utils/embed.js
+++ utils/embed.js
@@ -1,9 +1,22 @@
 const { RichEmbed } = require('discord.js')
 
 class Embed extends RichEmbed {
-  constructor () {
+  constructor (type) {
     super()
-    this.color = 8978431
+
+    const t = type ? type.trim().toLowerCase() : 'default'
+    switch(t) {
+      case 'error':
+      case 'err':
+        this.color = 16736609
+        return
+      case 'warn':
+        this.color = 16756838
+        return
+      default:
+        this.color = 8978431
+        return
+    }
   }
 }
 
