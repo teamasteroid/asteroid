66496c67636b7b895f3e5d4bed3388a540a59cc4
Kim Kyu San <developerstarlight@gmail.com>
2019-12-31 16:14:19 +0900

    temp region json

---
+++ command/game/region/region.js
@@ -0,0 +1,14 @@
+const regions = {
+   mecury: {
+     name: '수성',
+     loc: 5790
+   },
+   venus: {
+    name: '화성',
+    loc: 10821
+   },
+   earth: {
+     name: '지구',
+     loc: 14960
+   }
+}
