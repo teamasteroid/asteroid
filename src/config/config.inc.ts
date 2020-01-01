77fd916040ad34065a39545de2da792d75db0d60
Kim Kyu San <developerstarlight@gmail.com>
2020-01-01 10:58:36 +0900

    config

---
+++ src/config/config.inc.ts
@@ -0,0 +1,21 @@
+/*
+const config: Config = {
+  admin: [""],
+  bot: {
+    token: "TOKEN",
+    prefix: "*",
+    shard: {
+      respawn: true,
+      count: "auto",
+      delay: 7500
+    }
+  },
+  db: {
+    host: "localhost",
+    port: 3306,
+    database: "asteroid",
+    user: "root",
+    password: "PASSWORD"
+  }
+}
+*/
