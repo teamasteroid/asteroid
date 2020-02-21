66fae1b07f8acfc86ee3130befb3cd6b297fc908
Kim Kyu San <developerstarlight@gmail.com>
2020-02-21 19:30:16 +0900

    temp event handler

---
+++ src/events/EventHandler.ts
@@ -0,0 +1,15 @@
+import { Client } from 'discord.js'
+
+class EventHandler {
+  _name: string
+
+  constructor(eventName: string) {
+    this._name = name
+  }
+
+  bind(client: Client, e: any) {
+    console.warn('No given event handler')
+  }
+}
+
+export default EventHandler
