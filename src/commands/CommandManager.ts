e3950c4dda646316364853c46549bd136194f3c1
Kim Kyu San <developerstarlight@gmail.com>
2020-02-24 13:34:51 +0900

    command register

--- src/commands/CommandManager.ts
+++ src/commands/CommandManager.ts
@@ -1,16 +1,34 @@
 import { Collection } from 'discord.js'
 import Command from './Command'
 
+import Invite from './basic/Invite'
+
 class CommandManager {
   static commands: Collection<string, Command>
   static aliases: Collection<string, Command>
 
-  commandExecutors: Command[] = []
-  categories: string[] = []
+  static commandExecutors: Command[] = []
+  static categories: string[] = []
 
   static init() {
     CommandManager.commands = new Collection()
     CommandManager.aliases = new Collection()
+
+    CommandManager.commandExecutors = [
+      new Invite()
+    ]
+
+    CommandManager.commandExecutors.forEach(ce => CommandManager.register(ce))
+  }
+
+  static register (command: Command) {
+    CommandManager.commands.set(command.meta.name, command)
+    console.log(`${command.meta.name} command set`)
+
+    command.meta.alias.forEach(a => {
+      CommandManager.aliases.set(a, command)
+      console.log(`${a} command set`)
+    })
   }
 }
 
