26d5a88966f0ad812e852dde6300b497d31a938e
Kim Kyu San <developerstarlight@gmail.com>
2020-02-26 10:53:58 +0900

    embed util

---
+++ src/commands/Embed.ts
@@ -0,0 +1,27 @@
+import { RichEmbed } from 'discord.js'
+
+class Embed extends RichEmbed {
+  constructor (type: string) {
+    super()
+
+    const t = type ? type.trim().toLowerCase() : 'default'
+    switch(t) {
+      case 'error':
+      case 'err':
+        this.color = 16736609
+        return
+      case 'warn':
+        this.color = 16756838
+        return
+      case 'random':
+        const rand = Math.floor(Math.random() * 0xffffff)
+        this.color = rand
+        return
+      default:
+        this.color = 8978431
+        return
+    }
+  }
+}
+
+export default Embed
