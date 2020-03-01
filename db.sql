f4ac062c6111f67a9286c03e49449ffd8b0126d5
Kim Kyu San <developerstarlight@gmail.com>
2020-03-01 14:55:18 +0900

    user sql

--- db.sql
+++ db.sql
@@ -1,15 +1,9 @@
 CREATE DATABASE IF NOT EXISTS `asteroid`
 USE `asteroid`;
 
-CREATE TABLE IF NOT EXISTS `lang` (
-  `id` bigint(20) DEFAULT NULL,
-  `lang` tinytext DEFAULT NULL,
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 CREATE TABLE IF NOT EXISTS `user` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `discord` tinytext NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
